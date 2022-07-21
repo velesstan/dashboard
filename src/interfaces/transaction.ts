d8e8a2f9c13994f51a933a49216259e3590bedc9
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2022-07-21 20:23:35 +0300

    Update interface

--- src/interfaces/transaction.ts
+++ src/interfaces/transaction.ts
@@ -1,8 +1,16 @@
 import { Holder, Product } from 'interfaces';
 
 import { BaseEntity } from './entity';
+import type {
+    WaybillActionType,
+    WaybillStatusType,
+    WaybillType,
+} from './waybill';
 
 export interface Transaction extends BaseEntity {
+    type: WaybillType;
+    action: WaybillActionType;
+    status: WaybillStatusType;
     product: Product;
     holder: Holder;
     quantity: number;
