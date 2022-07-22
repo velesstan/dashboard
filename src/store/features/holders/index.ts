c78cf46eccb0c329bee7baa39fbe1ce1ea3c915b
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2022-07-22 14:58:42 +0300

    Update rtk query

--- src/store/features/holders/index.ts
+++ src/store/features/holders/index.ts
@@ -8,11 +8,23 @@ export const holdersApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: `${BASE_URL}/holders`,
     }),
+    tagTypes: ['Holder'],
     endpoints: builder => ({
         getHolders: builder.query<Holder[], void>({
             query: () => `/`,
+            providesTags: ['Holder'],
+        }),
+        updateHolder: builder.mutation<Holder, Partial<Holder>>({
+            query: ({ _id, ...update }) => ({
+                url: `/${_id}`,
+                method: 'PUT',
+                body: update,
+            }),
+            invalidatesTags: (result, error, { _id }) => [
+                { type: 'Holder', id: _id },
+            ],
         }),
     }),
 });
 
-export const { useGetHoldersQuery } = holdersApi;
+export const { useGetHoldersQuery, useUpdateHolderMutation } = holdersApi;
