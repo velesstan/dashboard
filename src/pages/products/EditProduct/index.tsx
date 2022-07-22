c188d6d5604f269f5948c7aaaafc5a015d166340
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2022-07-22 17:35:17 +0300

    Crud products

--- src/pages/products/EditProduct/index.tsx
+++ src/pages/products/EditProduct/index.tsx
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { Select } from 'antd';
 
 import { Page } from 'components/Page';
 import { Product } from 'interfaces';
 import {
     useCreateProductMutation,
+    useGetCategoriesQuery,
     useUpdateProductMutation,
 } from 'store/features';
 import { CommonForm } from 'components/CommonForm';
 
 export const EditProduct: React.FC = () => {
     const { state } = useLocation() as { state: { entity: Product } };
+
+    const categories = useGetCategoriesQuery().data || [];
 
     const [createProduct] = useCreateProductMutation();
     const [updateProduct] = useUpdateProductMutation();
@@ -31,7 +35,48 @@ export const EditProduct: React.FC = () => {
 
     return (
         <Page title='Продукты'>
-            <CommonForm entity={entity} onSave={onSave} />
+            <CommonForm
+                entity={entity}
+                onSave={onSave}
+                transformEntity={entity => ({
+                    ...entity,
+                    category: entity?.category?._id,
+                })}
+                fields={[
+                    {
+                        label: 'Категория',
+                        name: 'category',
+                        component: (
+                            <Select
+                                options={categories.map(category => ({
+                                    label: category.title,
+                                    value: category._id,
+                                }))}
+                            />
+                        ),
+                    },
+                    {
+                        label: 'СИ',
+                        name: 'unit',
+                    },
+                    {
+                        label: 'Код',
+                        name: 'code',
+                    },
+                    {
+                        label: 'Название',
+                        name: 'title',
+                    },
+                    {
+                        label: 'Цена розн.',
+                        name: 'price_retail',
+                    },
+                    {
+                        label: 'Цена опт.',
+                        name: 'price_wholesale',
+                    },
+                ]}
+            />
         </Page>
     );
 };
--- src/pages/products/ProductsList/columns.ts
+++ src/pages/products/ProductsList/columns.ts
@@ -2,12 +2,28 @@ import type { DataColumn, Product } from 'interfaces';
 
 const columns: Array<DataColumn<Product>> = [
     {
-        title: 'Название',
-        dataIndex: 'title',
+        title: 'Категория',
+        dataIndex: ['category', 'title'],
+    },
+    {
+        title: 'СИ',
+        dataIndex: 'unit',
     },
     {
         title: 'Код',
         dataIndex: 'code',
+    },
+    {
+        title: 'Название',
+        dataIndex: 'title',
+    },
+    {
+        title: 'Цена розн.',
+        dataIndex: 'price_retail',
+    },
+    {
+        title: 'Цена опт.',
+        dataIndex: 'price_wholesale',
     },
 ];
 
